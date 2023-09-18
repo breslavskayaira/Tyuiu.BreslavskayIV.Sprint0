3ace403e98d24b1bcc9521fa787559fd4ad1059d
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-18 17:22:50 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task7.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint0Task7V4Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint0Task7V4Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task7* date = new Service7();
+			int k = 100000;
+			int a = 1;
+			int c;
+			c = date->Rezalt(k, a);
+			Assert::AreEqual(1, c);
+		}
+	};
+}
