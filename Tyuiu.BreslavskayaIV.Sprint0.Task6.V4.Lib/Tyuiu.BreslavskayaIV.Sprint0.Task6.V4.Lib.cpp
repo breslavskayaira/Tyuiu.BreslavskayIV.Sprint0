30edd238166ff5cffa3064e62ca87a2239aed24f
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-18 17:20:49 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task6.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task6.V4.Lib.cpp
@@ -0,0 +1,18 @@
+﻿// Tyuiu.BreslavskayaIV.Sprint0.Task6.V4.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "math.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+// TODO: This is an example of a library function
+class Servise6 : public ISprint0Task6
+{
+	virtual int Calculate(int a, int b) override
+	{
+		int date;
+		date=(5*a)/(7+b);
+		return date;
+	}
+
+};
