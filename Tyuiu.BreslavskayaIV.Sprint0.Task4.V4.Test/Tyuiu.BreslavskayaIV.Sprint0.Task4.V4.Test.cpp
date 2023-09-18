11c9c95b2b18c08f00390e645e34df87cf396d7b
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-18 17:18:08 +0300

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Test.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Lib/Tyuiu.BreslavskayaIV.Sprint0.Task4.V4.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVSprint0Task4V4Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVSprint0Task4V4Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task4* date = new Service4();
+			int a = 2;
+			int b = 3;
+			int c = 4;
+			int d = 7;
+			int f;
+			f = date-> Calculate(a, b, c, d);
+			Assert::AreEqual(34, f);
+		}
+	};
+}
